32f237505ef19bcab76c1533f3b23e6312b99f8d
Volodymyr Kopytin <volodymyr.kopytin@gmail.com>
2024-06-01 18:55:33 +0200

    Fixing healthcheck

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 app.UsePathBase("/api");
