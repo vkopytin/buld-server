670753a90562ef3f40d6b1f59fd94dc9146c9e68
Volodymyr Kopytin <volodymyr.kopytin@gmail.com>
2024-06-01 18:35:16 +0200

    initial

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UsePathBase("/api");
+app.MapHealthChecks("/health");
+app.Run();
