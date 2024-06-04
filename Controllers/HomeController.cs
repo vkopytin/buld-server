395b74b6cbc1e7f2a00bfa3c0703711b0f900da5
Volodymyr Kopytin <volodymyr.kopytin@gmail.com>
2024-06-04 11:02:11 +0200

    Setup OpenId connect auth

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,7 +1,5 @@
 using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Primitives;
 
 namespace Controllers;
 
@@ -15,5 +13,24 @@ public class HomeController : ControllerBase
   public IActionResult Data()
   {
     return Ok(new { test = "test" });
+  }
+
+  [Authorize]
+  [HttpGet]
+  [ActionName("index")]
+  public IActionResult Index()
+  {
+    var claims = new Dictionary<string, Dictionary<string, string>>();
+
+    foreach (var claim in User.Claims)
+    {
+      claims.Add(claim.Type, new Dictionary<string, string>()
+      {
+        ["value"] = claim.Value,
+        ["Issuer"] = claim.Issuer,
+      });
+    }
+
+    return Ok(claims);
   }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,18 @@
-﻿using System.Text;
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using System.Text.Json;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Logging;
 using Microsoft.IdentityModel.Tokens;
+
+IdentityModelEventSource.ShowPII = true;
 
 var builder = WebApplication.CreateBuilder(args);
 var jwtSecretKey = builder.Configuration["JWT:SecretKey"] ?? throw new Exception("appsettings config error: JWT secret key is null");
@@ -30,10 +38,12 @@ builder.Services.AddCors(options =>
 });
 builder.Services.AddHttpClient<IdmAccessTokenAuthSchemeHandler>();
 builder.Services.AddControllers();
+JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+builder.Services.AddHttpContextAccessor();
 builder.Services.AddAuthorization(options =>
 {
   var scopes = new[] {
-    "read:data",
+    "read:user-info",
     "update:billing_settings",
     "read:customers",
     "read:files"
@@ -46,18 +56,39 @@ builder.Services.AddAuthorization(options =>
       )
     )
   );
-}).AddAuthentication("IdmAccessToken")
-.AddScheme<IdmAccessTokenAuthSchemeOptions, IdmAccessTokenAuthSchemeHandler>(
-      "IdmAccessToken",
-      opts => { }
-);
+}).AddAuthentication(config =>
+{
+  config.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+  config.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+})
+.AddCookie(CookieAuthenticationDefaults.AuthenticationScheme)
+.AddOpenIdConnect(OpenIdConnectDefaults.AuthenticationScheme, options =>
+{
+  // this is my Authorization Server Port
+  options.Authority = "https://localhost:5000";
+  options.ClientId = "platformnet6";
+  options.ClientSecret = "qarxcmlxcahildalknv-qarxcmlxcahildalknv-qarxcmlxcahildalknv-123";
+  options.ResponseType = "code";
+  options.CallbackPath = "/signin-oidc";
+  options.SaveTokens = true;
+  options.UseSecurityTokenValidator = true;
+  options.TokenValidationParameters = new TokenValidationParameters
+  {
+    SignatureValidator = delegate (string token, TokenValidationParameters validationParameters)
+    {
+      var jwt = new JwtSecurityToken(token);
+      return jwt;
+    },
+  };
+});
 builder.Services.AddHealthChecks();
 builder.Services.AddSingleton<IAuthorizationHandler, RequireScopeHandler>();
 
 var app = builder.Build();
-
 app.UsePathBase("/api");
 app.UseCors(apiCorsPolicy);
+app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
 app.MapControllers();
 app.MapHealthChecks("/health");
