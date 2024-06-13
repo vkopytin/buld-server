99644b5269a27ef161ae9fe8efa77b7b0389c745
Volodymyr Kopytin <volodymyr.kopytin@gmail.com>
2024-06-13 21:01:31 +0200

    Updating required fields for the User record

--- Db/Models/User.cs
+++ Db/Models/User.cs
@@ -8,9 +8,9 @@ public class User
   [Key]
   public ObjectId Id { get; set; }
 
-  public string UserName { get; set; } = "";
-  public string Name { get; set; } = "";
-  public string Role { get; set; } = "Everyone";
-  public bool IsActive { get; set; } = false;
-  public string Password { get; set; } = "";
+  public string UserName { get; set; }
+  public string? Name { get; set; }
+  public string? Role { get; set; }
+  public bool IsActive { get; set; }
+  public string? Password { get; set; }
 }
