149529c1a1b52e8db26aada7c0af063d63c6a8c8
Volodymyr Kopytin <4933561+vkopytin@users.noreply.github.com>
2025-11-13 09:40:18 +0100

    Fixing Entity key id map error

--- Db/Models/RoleRecord.cs
+++ Db/Models/RoleRecord.cs
@@ -1,10 +1,12 @@
 using System.ComponentModel.DataAnnotations;
+using MongoDB.Bson.Serialization.Attributes;
 
 namespace Account.Db.Records;
 
 public class RoleRecord
 {
   [Key]
+  [BsonId]
   public string RoleName { get; set; } = "";
   public int Permissions { get; set; } = 0; // Value from enum RolePermissions
 }
